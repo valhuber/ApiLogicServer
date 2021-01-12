0503a156cad1ea5e5a50a1701a347d44cf0999a0
valhuber <valjhuber@gmail.com>
2021-01-12 10:18:25 -0800

    trying to set up db for models... hand import maybe worked

--- app_logic_server/create_server.py
+++ app_logic_server/create_server.py
@@ -728,7 +728,7 @@ def run(ctx, project_name: str, db_url: str, favorites: str, non_favorites: str)
     create_project_debug = True
     if create_project_debug:
         clone_prototype_project(project_name)
-        create_models(db_url, project_name)
+        create_models(db_url, project_name)  # calls sqlacodegen
 
     """
         Create views.py file from db, models.py
