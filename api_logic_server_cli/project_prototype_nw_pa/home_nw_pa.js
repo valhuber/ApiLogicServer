b61802b0f56acf117ca841b7c67fcea6bb079be2
valhuber <valjhuber@gmail.com>
2022-01-01 16:47:03 -0800

    revised and simplified home page, already deployed to PA

--- api_logic_server_cli/project_prototype_nw_pa/home_nw_pa.js
+++ api_logic_server_cli/project_prototype_nw_pa/home_nw_pa.js
@@ -1,26 +1,31 @@
-const sla_doc = '<div class="MuiTypography-root"><center><h1>Welcome to API Logic Server - Sample</h1></center><br>' +
-    '<h2>Automatic Admin App, Designed For</h2>' +
-    '<ul><li>Instant Business User Collaboration - Working Software <i>Now</i></li>' +
-    '<li>Back Office Data Maintenance</li></ul>' +
-    '<br><h2>Key Features</h2>' +
+const sla_doc =
+    '<div class="MuiTypography-root jss4" style="color: rgba(0, 0, 0, 0.66)">' +
+    '<div style="text-align:center">' +
+    '<h2>Welcome to API Logic Server - PythonAnywhere Sample</h2>' +
+    '</div><br>' +
+    '<br><h3><a class="custom" style="color: #3f51b5;"  rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md/" target="_blank">API Logic Server</a> ' +
+    'creates <i>customizable</i> systems, instantly from your database:</h3>' +
+    '<h4>1. Automatic Admin App, Designed For</h4>' +
     '<ul>' +
-    '<li><strong>Multi-page:</strong> screen transitions</li>' +
-    '<li><strong>Multi-table:</strong> child grids, parent joins</li>' +
-    '<li><strong>Logic aware:</strong> multi-table derivations and constraints, extensible with Python events for email, messages, etc</li>' +
-    '<li><strong>Customizable:</strong> see ui/admin/admin.yaml.  Use SAFRS API for custom apps.</li></ul>' +
-    '<br><h2>Resources</h2>' +
-    '<ul>' +
-    '<li><a class="custom" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Admin-Tour" target="_blank">Tutorial</a></li>' +
-    '<li><a class="custom" rel="nofollow" href="https://apilogicserver.pythonanywhere.com/api" target="_blank">Swagger</a></li>' +
-    '<li><a class="custom" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md/" target="_blank">API Logic Server Docs</a></li>' +
-    '<li><a class="custom" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Working-with-the-Admin-App/" target="_blank">Admin App Customization Docs</a></li>' +
+    '   <li>For instant collaboration and Back Office data maintenance</li>' +
+    '   <li>Rich functionality: multi-page, multi-table</li>' +
+    '   <li><a class="custom" style="color: #3f51b5;"  rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Admin-Tour/" target="_blank">Explore</a> this Admin App, ' +
+    '        and how to <a class="custom" style="color: #3f51b5" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md#admin-app" target="_blank">customize it</a></li>' +
     '</ul>' +
-    '<br><h2>Sample Customizations</h2>' +
-    'While the Sample Application is mainly created from the data model, it also includes these customizations you can explore:' +
+    '<h4>2. API, with <a class="custom" style="color: #3f51b5;"  rel="nofollow" href="http://localhost:5656/api" target="_blank">Swagger</a></h4>' +
     '<ul>' +
-    '<li><a class="custom" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md#admin-app" target="_blank">Customizing your admin app</a></li>' +
-    '<li><a class="custom" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md#api-customization" target="_blank">Customizing your api</a></li>' +
-    '<li><a class="custom" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md#logic" target="_blank">Customizing your logic</a></li>' +
+    '   <li>For custom app dev, integration</li>' +
+    '   <li>Rich functionality: endpoint for each table, with filtering, pagination, related data</li>' +
+    '   <li><a class="custom" style="color: #3f51b5" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md#api-customization" target="_blank">Customizable</a>: add your own endpoints</li>' +
+    '</ul>' +
+    '<h4>3. Business Logic, for <span class="JoinedField" title="Often nearly half the app -- automation required"><span>backend processing</span> </span></h4>' +
+    '<ul>' +
+    '   <li>Spreadsheet-like rules for multi-table derivations and constraints</li>' +
+    '   <li>Extensible with Python events for email, messages, etc</li>' +
+    '   <li><a class="custom" style="color: #3f51b5" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Logic:-Rules-plus-Python" target="_blank">Explore</a> ' +
+    '       how logic can meaningfully improve ' +
+    '       <a class="custom" style="color: #3f51b5" rel="nofollow" href="https://github.com/valhuber/LogicBank/wiki/by-code" title="Rules are 40X more concise than code, and address over 95% of database logic" target="_blank">conciseness</a> ' +
+    '       and quality</li>' +
     '</ul>' +
     '</div>'
 
