bf828cdb78fcc58236e77eb7025828353c64d571
valhuber <valjhuber@gmail.com>
2022-01-02 17:20:15 -0800

    tweak PA home page

--- api_logic_server_cli/project_prototype_nw_pa/home_nw_pa.js
+++ api_logic_server_cli/project_prototype_nw_pa/home_nw_pa.js
@@ -1,15 +1,19 @@
 const sla_doc =
     '<div class="MuiTypography-root jss4" style="color: rgba(0, 0, 0, 0.66)">' +
     '<div style="text-align:center">' +
-    '<h2>Welcome to API Logic Server - PythonAnywhere Sample</h2>' +
+    '<h2>Welcome to API Logic Server - Sample System</h2>' +
     '</div><br>' +
-    '<h3><a class="custom" style="color: #3f51b5;"  rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md/" target="_blank">API Logic Server</a> ' +
-    'creates <i>customizable</i> systems, instantly from your ' +
-    '<a class="custom" style="color: #3f51b5;"  rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Sample-Database" target="_blank">database:</a>' +
-    '</h3>' +
-    '<h4>1. Automatic Admin App</h4>' +
+    '<p>' +
+    'The ' +
+    '<a class="custom" style="font-family: Courier; font-weight:bold; " rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/blob/main/README.md/" title="Instead of weeks or months" target="_blank">ApiLogicServer</a>' +
+    '<a class="custom" style="font-family: Courier; font-weight:bold; text-decoration: none;" rel="nofollow" href="#" title="Instead of weeks or months" target="_blank"> create-and-run</a>' +
+    ' command created this customizable system - automatically from your ' +
+    '<a class="custom" style="color: #3f51b5;"  rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Sample-Database" target="_blank">database</a>' +
+    ' - consisting of:' +
+    '</p>' +
+    '<h4>1. Automatic Admin App (you are running it now)</h4>' +
     '<ul>' +
-    '   <li>For instant collaboration and Back Office data maintenance</li>' +
+    '   <li>For instant collaboration and back office data maintenance</li>' +
     '   <li>Rich functionality: multi-page, multi-table</li>' +
     '   <li><a class="custom" style="color: #3f51b5;"  rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Admin-Tour/" target="_blank">Explore</a> this Admin App, ' +
     '        and how to <a class="custom" style="color: #3f51b5" rel="nofollow" href="https://github.com/valhuber/ApiLogicServer/wiki/Working-with-the-Admin-App" target="_blank">customize it</a></li>' +
