62d711f180163e5ad8f3437e2d452a172fb123fe
Thomas Pollet <thomas.pollet@gmail.com>
2022-01-14 11:31:50 +0100

    model loading strat

--- api_logic_server_cli/project_prototype/api/expose_api_models.py
+++ api_logic_server_cli/project_prototype/api/expose_api_models.py
@@ -4,10 +4,10 @@ import importlib
 import pathlib
 import logging as logging
 # use absolute path import for easier multi-{app,model,db} support
-db_spec =importlib.util.spec_from_file_location("database", pathlib.Path(__file__).parent.parent / "database.py")
-database = importlib.util.module_from_spec(db_spec)
-from database import models
-
+db_py = pathlib.Path(__file__).parent.parent / "database/models.py"
+db_spec =importlib.util.spec_from_file_location("models", db_py.resolve())
+models = importlib.util.module_from_spec(db_spec)
+db_spec.loader.exec_module(models)
 
 app_logger = logging.getLogger('api_logic_server_app')
 app_logger.info("api/expose_api_models.py - endpoint for each table")
