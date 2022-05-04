40b0a17a22f23565b5c274853db5d500a580bbbe
valhuber <valjhuber@gmail.com>
2022-05-03 17:44:15 -0700

    rebuild tests, notes

--- tests/creation_tests/create-servers.sh
+++ tests/creation_tests/create-servers.sh
@@ -6,10 +6,10 @@ export ApiLogicServer=${install}/ApiLogicServer
 if [ $# -eq 0 ]
     then
         echo ""
-        echo "Creates venv and servers at ${ApiLogicServer}"
+        echo "Creates servers at ${ApiLogicServer}"
         # ls ${ApiLogicServer}
         echo ""
-        echo "  sh create-servers.sh [ create | venv ]"
+        echo "  sh create-servers.sh create"
         # ls ${ApiLogicServer}
         echo ""
         exit 0
@@ -36,19 +36,4 @@ if [ "$1" = "create" ]
         popd
 
         echo "\n\n******\nuse vsc to run api_logic_server_run.py (Launch Config: Python: Current File) "
-    fi
-
-if [ "$1" = "venv" ]
-    then
-        read -p "Press [Enter] to remove / reinstall ${ApiLogicServer}/venv (deactivate first) > "
-
-        rm -r ${ApiLogicServer}/venv
-        python3 -m venv ${ApiLogicServer}/venv
-
-        echo ""
-        echo "venv created, now..."
-        echo "  source ${ApiLogicServer}/venv/bin/activate  # ;a"
-        echo "  pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple ApiLogicServer==5.00.04"
-
-        echo " "
     fi
