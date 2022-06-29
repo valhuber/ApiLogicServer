08c6e59abb0afeae2b7e2bec2f4312b4982bed96
Val Huber <valjhuber@gmail.com>
2022-06-29 10:06:49 -0700

    minor

--- tests/creation_tests/docker-commands.sh
+++ tests/creation_tests/docker-commands.sh
@@ -8,6 +8,6 @@ ApiLogicServer create --project_name=/localhost/chinook --db_url=mysql+pymysql:/
 
 ApiLogicServer create --project_name=/localhost/postgres --db_url=postgresql://postgres:p@postgresql-container/postgres
 
-ApiLogicServer create --project_name=/localhost/sqlserver --db_url='mssql+pyodbc://sa:posey386!@sqlsvr-container:1433/NORTHWND?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=no'
+ApiLogicServer create --project_name=/localhost/sqlserver --db_url='mssql+pyodbc://sa:posey386!@sqlsvr-container:1433/NORTHWND?driver=ODBC+Driver+18+for+SQL+Server&trusted_connection=no&Encrypt=no'
 
 ApiLogicServer create --project_name=/localhost/ApiLogicServer --db_url=
