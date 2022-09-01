9ed19ea6e81a97d42c203f6e5728ed0f59522253
Val Huber <valjhuber@gmail.com>
2022-08-31 19:24:07 -0700

    06.00.04: Codespaces

--- api_logic_server_cli/cli.py
+++ api_logic_server_cli/cli.py
@@ -13,10 +13,10 @@ See end for key module map quick links...
 
 """
 
-__version__ = "6.00.03"
+__version__ = "6.00.04"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t08/31/2022 - 06.00.03: Codespaces - support create to '.' or './', preserve readme \n"\
+    "\t08/31/2022 - 06.00.04: Codespaces - support create to '.' or './', preserve readme \n"\
     "\t08/29/2022 - 06.00.01: Admin App show_when & cascade add. Simplify Codespaces swagger url & use default config \n"\
     "\t08/15/2022 - 05.03.34: Remove Postgres driver from local install, Fix ApiLogicServer run fails (Issue 45) \n"\
     "\t07/24/2022 - 05.03.26: api_logic_server_run refactor, codespaces support \n"\
@@ -351,11 +351,13 @@ def create_project_with_nw_samples(project_directory: str, project_name: str,
                 if merge_into_prototype:
                     # tmpdirname = tempfile.TemporaryDirectory() 
                     recursive_overwrite(project_directory, str(tmpdirname))
+                    delete_dir(str(Path(str(tmpdirname)) / ".devcontainer"), "")
                     delete_dir(str(Path(str(tmpdirname)) / "api"), "")
                     delete_dir(str(Path(str(tmpdirname)) / "database"), "")
-                    delete_dir(str(Path(str(tmpdirname)) / "ui"), "")
+                    delete_dir(str(Path(str(tmpdirname)) / "logic"), "")
                     delete_dir(str(Path(str(tmpdirname)) / "test"), "")
                     delete_dir(str(Path(str(tmpdirname)) / "ui"), "")
+                    os.remove(str(Path(str(tmpdirname)) / "api_logic_server_run.py"))
                     delete_dir(realpath(project_directory), "")
                     recursive_overwrite(from_dir, project_directory)  # ApiLogic Proto -> new project
                 else:
