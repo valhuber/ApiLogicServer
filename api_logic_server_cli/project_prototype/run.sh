ca2086092b8c85ffc5c5002a8df0b1e0700ea10f
Val Huber <valjhuber@gmail.com>
2022-11-09 11:07:06 -0800

    6.2.38 (admin update failing mac)

--- api_logic_server_cli/project_prototype/run.sh
+++ api_logic_server_cli/project_prototype/run.sh
@@ -7,7 +7,12 @@ contains()
     (*) false;;
   esac
 
-echo "\nAPI Logic Project Runner 1.0 Here ($# arg(s): $1)"
+
+debug() {
+  echo "$1"
+}
+
+debug "\nAPI Logic Project Runner 1.0 Here ($# arg(s): $1)"
 if [ $# -eq 1 ]
   then
     if contains "help" $1; then
@@ -18,18 +23,18 @@ if [ $# -eq 1 ]
       echo "    $ - use the current venv\n"
       exit 0
     elif [ "$1" == "$" ]; then
-      echo ".. Using existing venv -- $VIRTUAL_ENV"
+      debug ".. Using existing venv -- $VIRTUAL_ENV"
     else
-      echo ".. Calling directory - venv from $PWD"
+      debug ".. Calling directory - venv from $PWD"
       cd $PWD
       . venv/bin/activate
     fi
   else
-    echo " "
+    debug " "
     if [ "${APILOGICSERVER_RUNNING}" = "DOCKER" ]; then
-      echo ".. Docker - no venv required"
+      debug ".. Docker - no venv required"
     else
-      echo ".. APILOGICSERVER_RUNNING=${APILOGICSERVER_RUNNING} - not Docker/Codespaces - activate venv"
+      debug ".. APILOGICSERVER_RUNNING=${APILOGICSERVER_RUNNING} - not Docker/Codespaces - activate venv"
       . venv/bin/activate
     fi
   fi
