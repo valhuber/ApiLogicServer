c4dd482f8ad78ffcf6b23ca0a227f4a1f6be0b4b
Val Huber <valjhuber@gmail.com>
2023-01-10 12:01:15 -0800

    7.0.4 - separate system/server from security

--- api_logic_server_cli/project_prototype/security/system/server_proxy.py
+++ api_logic_server_cli/project_prototype/security/system/server_proxy.py
@@ -5,13 +5,18 @@ authentication_provider = config.Config.SECURITY_PROVIDER
 
 class Server():
 
-    _current_user = None
+    _JWT = None
+    """ current user's JWT (user, roles) """
     
     @classmethod
     def current_user_from_JWT(cls):
         """ 
         STUB for authentication - presumably returned from JWT saved during login
         """
-        if Server._current_user is None:
-            Server._current_user = authentication_provider.get_user("aneu", "")
-        return Server._current_user
+        if Server._JWT is None:
+            Server.login("aneu", "")
+        return Server._JWT
+    
+    @classmethod
+    def login(cls, id, password):
+        Server._JWT = authentication_provider.get_user("aneu", "")
