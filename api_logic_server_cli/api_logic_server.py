4093cc73dcc82cd7e451db7ffda8fb25073b7c68
Val Huber <valjhuber@gmail.com>
2023-02-11 13:19:23 -0800

    07.00.65 - defer safrs 3 until later branch

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "07.00.64"
+__version__ = "07.00.65"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t02/11/2023 - 07.00.64: Updated venv/setup, no FAB, threaded, nw-, add-sec/cust, app-lite docker, std log, tut, org-docs, logic \n"\
+    "\t02/11/2023 - 07.00.66: Updated venv/setup, no FAB, threaded, nw-, add-sec/cust, app-lite docker, std log, tut, org-docs, logic \n"\
     "\t01/10/2023 - 07.00.04: Portable projects, server_proxy  \n"\
     "\t01/06/2023 - 07.00.00: Multi-db, sqlite test dbs, tests run, security prototype, env config  \n"\
     "\t12/21/2022 - 06.05.00: Devops, env db uri, api endpoint names, git-push-new-project  \n"\
