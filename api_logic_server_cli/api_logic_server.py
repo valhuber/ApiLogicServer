cab64c6a810cf8ce8d3bdfd65e4f4068ab05b323
Val Huber <valjhuber@gmail.com>
2023-02-12 21:22:21 -0800

    idea run configs

--- api_logic_server_cli/api_logic_server.py
+++ api_logic_server_cli/api_logic_server.py
@@ -12,10 +12,10 @@ ApiLogicServer CLI: given a database url, create [and run] customizable ApiLogic
 Called from api_logic_server_cli.py, by instantiating the ProjectRun object.
 '''
 
-__version__ = "07.00.71"
+__version__ = "07.00.72"
 recent_changes = \
     f'\n\nRecent Changes:\n' +\
-    "\t02/12/2023 - 07.00.71: Updated venv/setup, no FAB, threaded, nw-, add-sec/cust, app-lite docker, std log, tut, org-docs, logic, safrs 3 \n"\
+    "\t02/12/2023 - 07.00.72: Updated venv/setup, no FAB, threaded, nw-, add-sec/cust, app-lite docker, std log, tut, org-docs, logic, safrs 3 \n"\
     "\t01/10/2023 - 07.00.04: Portable projects, server_proxy  \n"\
     "\t01/06/2023 - 07.00.00: Multi-db, sqlite test dbs, tests run, security prototype, env config  \n"\
     "\t12/21/2022 - 06.05.00: Devops, env db uri, api endpoint names, git-push-new-project  \n"\
