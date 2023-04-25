5b88573a2709032b032b842215d2500f91cf2722
Val Huber <valjhuber@gmail.com>
2023-04-24 17:46:15 -0700

    08.02.05: virt attrs (Issue 56), safrs 3.0.2

--- api_logic_server_cli/project_prototype_nw/database/customize_models.py
+++ api_logic_server_cli/project_prototype_nw/database/customize_models.py
@@ -2,6 +2,8 @@ from database import models
 import logging
 from safrs import jsonapi_attr
 from sqlalchemy.orm import relationship, remote, foreign
+from functools import wraps # This convenience func preserves name and docstring
+import decimal as decimal
 
 """
     If you wish to drive models from the database schema,
@@ -10,6 +12,22 @@ from sqlalchemy.orm import relationship, remote, foreign
 """
 
 app_logger = logging.getLogger(__name__)
+
+def add_method(cls):
+  """
+  Decorator to add method to class, e.g., derived attribute ProperSalary.
+
+  Thanks to: https://mgarod.medium.com/dynamically-add-a-method-to-a-class-in-python-c49204b85bd6
+  """
+  def decorator(func):
+    @wraps(func) 
+    def wrapper(self, *args, **kwargs): 
+      return func(*args, **kwargs)
+    
+    setattr(cls, func.__name__, wrapper)
+    # Note we are not binding func, but wrapper which accepts self but does exactly the same as func
+    return func # returning func means func can still be used normally
+  return decorator
 
 # add relationship: https://docs.sqlalchemy.org/en/13/orm/join_conditions.html#specifying-alternate-join-conditions
 models.Employee.Manager = relationship('Employee', cascade_backrefs=True, backref='Manages',
@@ -32,21 +50,28 @@ They must be manually added to your ui/admin/admin.yaml, e.g.
 
 """
 
-
 # add derived attribute: https://github.com/thomaxxl/safrs/blob/master/examples/demo_pythonanywhere_com.py
+@add_method(models.Employee)
 @jsonapi_attr
-def proper_salary(row):  # type: ignore [no-redef]
-    if not hasattr(row, "_proper_salary"):
-        row._proper_salary = row.Salary * 1.25  # create the attr with default value
-    return row._proper_salary
+def __proper_salary__(self):  # type: ignore [no-redef]
+    import database.models as models
+    if isinstance(self, models.Employee):
+        import decimal
+        rtn_value = self.Salary
+        rtn_value = decimal.Decimal('1.25') * rtn_value
+        self._proper_salary = int(rtn_value)
+        return self._proper_salary
+    else:
+        print("class")
+        return None
 
-
-@proper_salary.setter
-def proper_salary(row, value):  # type: ignore [no-redef]
-    row._proper_salary = value
-    # print(f'_proper_salary={row._proper_salary}')
+@add_method(models.Employee)
+@__proper_salary__.setter
+def _proper_salary(self, value):  # type: ignore [no-redef]
+    self._proper_salary = value
+    print(f'_proper_salary={self._proper_salary}')
     pass
 
-models.Employee.ProperSalary = proper_salary
-
+models.Employee.ProperSalary = __proper_salary__
+      
 app_logger.info("..database/customize_models.py: models.Employee.Manager(manages), Employee.ProperSalary")
