de2706063c8231a5a498a0da81ebfaa4b9abf49c
Val Huber <valjhuber@gmail.com>
2023-06-08 10:34:51 -0700

    running with proper pips; probable dialect issues

--- api_logic_server_cli/project_prototype/database/db_debug.py
+++ api_logic_server_cli/project_prototype/database/db_debug.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine, inspect, MetaData
 from sqlalchemy.orm import Session
 import models
 
-    # ******************************************
-    # Use this to inspect SQLAlchemy connection
-    # ******************************************
+    # *******************************************
+    # Use this to inspect SQLAlchemy2 connection
+    # *******************************************
 
 def print_meta(meta):
     print("\n\nmeta.sorted_tables (meta = models.metadata.sorted_tables)\n")
@@ -14,22 +14,17 @@ def print_meta(meta):
         for each_column in each_table.columns:
             print(f'\t{each_column.name}')
 
-db_url = "replace_db_url"
+db_url = "sqlite:////Users/val/dev/servers/ApiLogicProject/database/db.sqlite"
 db_url = "sqlite:////Users/val/dev/servers/ApiLogicProject/database/nw-gold.sqlite"
 
 e = sqlalchemy.create_engine(db_url)
 inspector = inspect(e)
-pk = inspector.get_pk_constraint("Category")
-cols = inspector.get_columns("Category")
 conn = e.connect()
-
-session_maker = sqlalchemy.orm.sessionmaker()
-session_maker.configure(bind=e)
-session = session_maker()
 
 with Session(e) as session:
     print(f'session: {session}')
-    metadata = MetaData(bind=e)
+    # metadata = MetaData(bind=e)
+    metadata = MetaData()  # SQLAlchemy2
     meta = models.metadata  # tables should show list of db tables
     print_meta(meta)
-    print(f'\nSqlite debug info\npk: {str(pk)}, \ncols: {str(cols)}\n')
+    print("\n\n")
